c2723e99d7d4d3e5da90b3e80be671e5bef2824c
Luís <59730229+d4k37@users.noreply.github.com>
2021-05-12 15:58:30 -0300

    up

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,11 +3,11 @@ import '../styles/repositories.scss';
 export function RepositoryItem(props){
     return(
         <li>
-        <strong>{props.repository.name ?? 'default'}</strong>
+        <strong>{props.repository.name}</strong>
         <p>{props.repository.description}</p>
 
 
-        <a href={props.repository.link}>
+        <a href={props.repository.html_url}>
             Acessar repositório
         </a>
     </li>
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,30 @@
 import {RepositoryItem} from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
 
-const repository = {
-    name: 'unform',
-    description: 'forms in react',
-    link: 'http://github.com/unform/unform'
-}
+// https://api.github.com/orgs/rocketseat/repos
+
+
 
 export function RepositoryList(){
+
+        const [repositories, setRepositories] = useState([]);
+
+        useEffect(()=>{
+            fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response=>response.json())
+            .then(data=>setRepositories(data))
+        }, [repositories])
+
+
     return(
         <section className="repository-list">
             <h1>Lista de repositorios  </h1>
 
                 <ul>
-                    <RepositoryItem repository={repository}/>
-                    <RepositoryItem repository={repository}/>
-                    <RepositoryItem repository={repository}/>
-                    <RepositoryItem repository={repository}/>
-                    <RepositoryItem repository={repository}/>
+                    {repositories.map(repository =>{
+                        return <RepositoryItem key={repository.name} repository={repository}/>
+                    })}
                 </ul>
         </section>
     )
